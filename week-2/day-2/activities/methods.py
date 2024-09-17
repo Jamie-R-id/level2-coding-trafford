386a8d19740a19d381a5a01685fb9abc82883972
Jamie-R-id <jreid7800@gmail.com>
2024-09-17 15:08:04 +0100

    week2 day1

---
+++ week-2/day-2/activities/methods.py
@@ -0,0 +1,29 @@
+x = "banana"
+
+# .upper() method returns a string where all characters are in upper case
+# print(x.upper())
+
+
+# # .lower() Converts a string into lower case
+# x="banana"
+# print(x.lower())
+
+
+# # .capitalize() Converts the first character to upper case
+# print(x.capitalize())
+
+
+# # .count() Returns the number of times a specified value occurs in a string
+# print(x.count("a"))
+
+
+# .find() Searches the string for a specified value and returns the position of where it was first found
+# print(x.find("a"))
+
+
+# #.replace() Returns a string where a specified value is replaced with a specified value
+# # print(x.replace("a","e"))
+
+
+# # .strip() Removes any leading, and trailing whitespaces or specified characters
+# # print(x.strip("ba"))
---
+++ week-2/day-2/activities/start.py
@@ -0,0 +1,41 @@
+# my_name = "Jamie"
+# age = 19
+# favorite_colour = "Red"
+
+# print(f"My name is {my_name} I am {age} years old and my favourite colour is {favorite_colour}")
+
+# breakfast = "weetabix"
+# lunch = "a meal containing egg"
+# dinner = "whatever is in the fridge"
+
+# print(f"For breakfast I eat {breakfast}. For lunch I eat {lunch}. For dinner I eat {dinner}")
+
+# breakfast = "porridge"
+# lunch = "pasta of some kind"
+# dinner = "dinner"
+
+# print(f"For breakfast I will eat {breakfast}. For lunch I will eat {lunch}. For dinner I will eat {dinner}")
+x = "x"
+o = "o"
+empty = ( "     |     |     ")
+
+# print(empty)
+# print(f"     |     |  {o} ")
+# print(empty)
+# print( "-----------------")
+# print(empty)
+# print(f"     |     |  {x}  ")
+# print(empty)
+# print( "-----------------")
+# print(empty)
+# print(f"  {x}  |     |  {o} ")
+# print(empty)
+
+import random
+i=0
+while True:
+    x=random.random()
+    i+=1
+    if x == 69:
+        print(i)
+        break
